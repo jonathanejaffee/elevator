31ebf9c9aaf64aaaa86d0372d72b589a815e8bac
Jonathan E Jaffee <jonathan.e.jaffee@gmail.com>
2024-03-03 20:39:02 -0700

    got ppl detect working

--- src/human_detection.cpp
+++ src/human_detection.cpp
@@ -1,11 +1,19 @@
 
 #include "human_detection.h"
+#include <vector>
 using namespace cv;
 using namespace cv::ml;
 using namespace hd;
 void HumanDetector::loadImage(string imgPath)
 {
     Mat img = imread(imgPath);
+    HOGDescriptor hog;
+    hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+    vector<Rect> found;
+    vector<double> weights;
+
+    hog.detectMultiScale(img, found, weights);
+    std::cout << "FOUND: " << found.size() << std::endl;
 }
 
 //void HumanDetector::detectHumans()
